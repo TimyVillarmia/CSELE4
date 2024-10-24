1280d0f08192fc9a1557048b584cf9f2ce807b0e
Timy Villarmia <timyvillarmia@gmail.com>
2024-10-24 17:36:51 +0800

    index page

--- CSELE4-Activity/CSELE4-Activity/Pages/Index.cshtml.cs
+++ CSELE4-Activity/CSELE4-Activity/Pages/Index.cshtml.cs
@@ -1,20 +1,26 @@
+using CSELE4_Activity.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace CSELE4_Activity.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly CSELE4_Activity.Data.CSELE4_ActivityContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+
+        public IndexModel(ILogger<IndexModel> logger, CSELE4_Activity.Data.CSELE4_ActivityContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
+        public IList<Product> Product { get; set; } = default!;
+        public async Task OnGetAsync()
         {
-
+            Product = await _context.Product.ToListAsync();
         }
     }
 }
